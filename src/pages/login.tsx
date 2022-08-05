846d3bc0714ae853fed6d2a3b351d54876e5f4e3
kauan777 <kauan7622@gmail.com>
2022-08-05 14:25:37 -0300

    add states

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,42 +1,50 @@
-import Link from 'next/link'
-import { Lock, UserCircle } from 'phosphor-react'
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
-import styles from '../../styles/login.module.scss'
-
+import Link from "next/link";
+import { Lock, UserCircle } from "phosphor-react";
+import { useState } from "react";
+import styles from "../../styles/login.module.scss";
 
 function Login() {
 
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
   return (
     <main className="container">
-        <div className={styles.boxLogin}>
-            <h1>Faça o login</h1>
-            <div className={styles.containerInput}>
-                <UserCircle size={24} color="#ffff" />
-                <input type="text" placeholder='Digite seu email'/>
-            </div>
-            <div className={styles.containerInput}>
-                <Lock size={24} color="#fff" />
-                <input  type="password" placeholder='Digite sua senha'/>
-            </div>
- 
-            <Link href="/">
-                <a className={styles.buttonSignIn}>
-                    Entrar
-                </a>
-            </Link>
-            <div className={styles.hr}>
-                <hr/>
-                <span>Ou</span>
-                <hr/>
-            </div>
-            <Link href="/signUp">
-                <a className={styles.buttonSignUp}>
-                    Cadastrar-se
-                </a>
-            </Link>
+      <div className={styles.boxLogin}>
+        <h1>Faça o login</h1>
+        <div className={styles.containerInput}>
+          <UserCircle size={24} color="#ffff" />
+          <input
+            type="text"
+            value={email}
+            placeholder="Digite seu email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
+        <div className={styles.containerInput}>
+          <Lock size={24} color="#fff" />
+          <input
+            type="password"
+            value={password}
+            placeholder="Digite sua senha"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <Link href="/">
+          <a className={styles.buttonSignIn}>Entrar</a>
+        </Link>
+        <div className={styles.hr}>
+          <hr />
+          <span>Ou</span>
+          <hr />
+        </div>
+        <Link href="/signUp">
+          <a className={styles.buttonSignUp}>Cadastrar-se</a>
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,36 +1,72 @@
-import Link from 'next/link'
-import { Lock, UserCircle } from 'phosphor-react'
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
-import styles from '../../styles/login.module.scss'
+import Link from "next/link";
+import { Detective, EnvelopeSimple, Lock } from "phosphor-react";
+import { useEffect, useState } from "react";
+import styles from "../../styles/login.module.scss";
 
+interface userProps {
+  nickname?: string;
+  email?: string;
+  password?: string;
+}
 
 function SignUp() {
+    const [nickname, setNickname] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [confirmPassword, setConfirmPassword] = useState<string>("")
+
+    const signUp = async () => {
+
+    }
+
 
   return (
     <main className="container">
-        <div className={styles.boxLogin}>
-            <h1>Tela de cadastro</h1>
-            <div className={styles.containerInput}>
-                <UserCircle size={24} color="#ffff" />
-                <input type="text" placeholder='Digite seu email'/>
-            </div>
-            <div className={styles.containerInput}>
-                <Lock size={24} color="#fff" />
-                <input  type="password" placeholder='Digite sua senha'/>
-            </div>
-            <div className={styles.containerInput}>
-                <Lock size={24} color="#fff" />
-                <input  type="password" placeholder='Confirme sua senha'/>
-            </div>
- 
-            <Link href="/login">
-                <a className={styles.buttonSignIn}>
-                    Cadastrar-se
-                </a>
-            </Link>
+      <div className={styles.boxLogin}>
+        <h1>Tela de cadastro</h1>
+        <div className={styles.containerInput}>
+          <Detective size={24} color="#ffff" />
+          <input 
+            type="text" 
+            placeholder="Digite seu nickname"
+            value={nickname}
+            onChange={(event) => setNickname(event.target.value)}
+          />
         </div>
+        <div className={styles.containerInput}>
+          <EnvelopeSimple size={24} color="#ffff" />
+          <input 
+            type="text" 
+            value={email}
+            placeholder="Digite seu email"
+           onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className={styles.containerInput}>
+          <Lock size={24} color="#fff" />
+          <input 
+            type="password"
+            value={password}
+            placeholder="Digite sua senha"
+            onChange={(event) => setPassword(event.target.value)}
+           />
+        </div>
+        <div className={styles.containerInput}>
+          <Lock size={24} color="#fff" />
+          <input 
+             type="password"
+             placeholder="Confirme sua senha"
+             value={confirmPassword}
+             onChange={(event) => setConfirmPassword(event.target.value)}
+             />
+        </div>
+
+        <Link href="/login">
+          <a className={styles.buttonSignIn} onClick={signUp}>Cadastrar-se</a>
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
